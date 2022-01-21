500d0894a59b60899926f76888d9be243b2e7432
Jaron Hoste <jaron@hoste.me>
2022-01-21 14:59:14 +0100

    Added admin routes and dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\Pages\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+require 'admin.php';
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/category/discussion/topic', [PageController::class, 'single'])->name('single');
+
+Route::get('discussion/create', [PageController::class, 'create'])->name('create');
+
+Route::get('dashboard/users', [PageController::class, 'users'])->name('users');
+
+Route::get('/dashboard/categories/index', [PageController::class, 'categoriesIndex'])->name('categories.index');
+Route::get('/dashboard/categories/create', [PageController::class, 'categoriesCreate'])->name('categories.create');
+
+Route::get('/dashboard/threads/index', [PageController::class, 'threadsIndex'])->name('threads.index');
+Route::get('/dashboard/threads/create', [PageController::class, 'threadsCreate'])->name('threads.create');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
